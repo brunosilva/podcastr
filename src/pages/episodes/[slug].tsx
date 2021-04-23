2e558d0cc87344392c56eae3c44083d98029003a
Bruno Silva <brs.rsilva@gmail.com>
2021-04-23 11:36:27 -0300

    content end of generate static page

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -28,6 +28,12 @@ type Episode = {
 export default function Episode({ episode }: EpisodeProps){
     const router = useRouter();
 
+    //isFallback = boolean (true or false) -> Se está em carregamento, da o return com a mensagem "Carregando..."
+    //Como está usando o fallback: 'blocking' não precisa desta validação
+    // if(router.isFallback){
+    //     return <p>Carregando...</p>
+    // }
+
     return (
         <div className={styles.episode}>
             <div className={styles.thumbnailContainer}>
@@ -66,9 +72,43 @@ export default function Episode({ episode }: EpisodeProps){
 
 //Obrigatório usar este método em toda página que usar geração estática e também em toda página que recebe parâmetro dinâmino no nome
 //ex.: [slug]
+//Principal momento que gera página estática da aplicação é no momento da build.
+//Se passar o paths:[] vazio, o next entende que não vai gerar de forma estática nenhum episodio da build
+//Cada episodio que deseja gerar de forma estática, deve ser enviado um objeto como params dentro do paths (pode receber mais de 1 parâmetro)
+//Definições fallback -> false    = se acessar episodio e não foi gerado pag estática não vai encontrar e vai retornar 404
+//                       true     = (Requisição acontece lado do client/browser) se acessar episodio e não foi gerado pag estática, o true faz com que 
+//                                  tente buscar o episodio para gerar a pág. 
+//                                  * Como não tem nada, isso pode demorar um pouco e dar erro no retorno por ainda não ter os dados carregados.
+//                                  * Para resolver faça import do hook useRouter
+//                       blocking = Ao clicar no link, só sera navegada para tela quando os dados tiverem sido carregados - Para SEO é a melhor opção, pois assim
+//                                  algum dado que depende de indexação já estará carregado.
+//                                  * 
+
+//conceito de fallback(true or blocking) -> Incremental static regeneration -> Permite gerar novas páginas conforme vão acessando e re-validar/re-gerar 
+//                                          páginas obsoletas (com prazo de validade vencido -> "revalidate")
 export const getStaticPaths: GetStaticPaths = async() => {
+
+    //faz uma chamada na API buscando os dois últimos episódios.
+    const { data } = await api.get('episodes', {
+        params:{
+          _limit: 2,
+          _sort: 'published_at',
+          _order: 'desc'
+        }
+    })
+
+    //Retorna params (id) para slug
+    const paths = data.map(episode => {
+        return {
+            params: {
+                slug: episode.id
+            }
+        }
+    })
+
+    //passa a const paths com o retorno dos episódios e ai vai gerar as páginas estáticas baseada nesse retorno.
     return {
-        paths: [],
+        paths,
         fallback:'blocking'
     }
 }
